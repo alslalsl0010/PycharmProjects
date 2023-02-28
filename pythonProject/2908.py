bd5a4f87e363b616a731a8186863e3cff01c2550
alslalsl0010 <alslalsl0010@naver.com>
2023-03-01 08:57:14 +0900

    initial

--- pythonProject/2908.py
+++ pythonProject/2908.py
@@ -6,7 +6,7 @@ b = "".join(reversed(b))
 print(max(int(a), int(b)))
 
 
-# join 주소접근시 or 문자열 합치기
+# join 주소접근시 or 문자열 합치기 조인도 소트랑 같이
 # b = [1, 23, 43, 2]
 # b.sort()  b 자체를 정렬
 # sorted(b) 정렬된 b 객체 (b는 변화 x)
