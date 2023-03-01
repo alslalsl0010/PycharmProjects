7599d8f3afe600695e5ee77662c39690d7680bff
alslalsl0010 <alslalsl0010@naver.com>
2023-03-02 01:47:20 +0900

    셀프 넘버 # set함수, str, int 활용 방법

--- pythonProject/5622.py
+++ pythonProject/5622.py
@@ -1,13 +1,11 @@
-h, m = map(int, input().split())
-timer = int(input())
+s = set(range(1, 10001))
 
-h += timer // 60
-m += timer % 60
+for a in range(1, 10001):
+    for b in str(a): # a = '1', '2'. '3', '4'
+        a += int(b) # a = 1234 + 1 + 2 + 3 + 4
+    s.discard(a)
 
-if m >= 60:
-    m -= 60
-    h += 1
-if h >= 24:
-    h -= 24
+for num in s:
+    print(num) # set 함수 대괄호와 콤마 없이 출력하기 위함
 
-print(h,m)
+# set함수, str, int 활용 방법
