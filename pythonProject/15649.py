2dd847514d34ce61a5a7eaaf71fa2441b3fc7ba8
alslalsl0010 <alslalsl0010@naver.com>
2023-03-05 11:10:33 +0900

    N과 M (dfs, join함수, 재귀활용)

--- pythonProject/15649.py
+++ pythonProject/15649.py
@@ -5,13 +5,13 @@ s = []
 
 def dfs():
     if len(s) == m:
-        print(' ' . join(map(str, s)))
+        print(' ' . join(map(str, s)))  # int형으로 하면 오류, string형으로 넣어줘야함
         return
 
     for i in range(1, n + 1):
         if i not in s:
             s.append(i)
-            dfs()
+            dfs()  # 기점으로 열 원소로 들어감
             s.pop()
 
 
