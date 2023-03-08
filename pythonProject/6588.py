ed8d9b265d9d83a0dc3b9209442b8ac65795b0e3
alslalsl0010 <alslalsl0010@naver.com>
2023-03-08 11:21:36 +0900

    골드바흐의 추측( 출력시 f함수 사용법 익히기)

--- pythonProject/6588.py
+++ pythonProject/6588.py
@@ -1,25 +1,30 @@
 import sys
-r = 1000000
-
-check = [True for _ in range(r)]
-
-for i in range(2, int(r**0.6)):
-    if check[i] == True:
-        for j in range(i*2, r, i):
-            if check[j] == True:
-                check[j] = False  # 에라토스테네스의 체
-
-
 input = sys.stdin.readline
 
+is_Prime = list([False]*1000001)
 
-while (True):  # 입력받은 수가 에라토스테네스의 체에 속하는지, 즉 소수인지
+is_Prime[2] = True
+
+for i in range(3, 1000001):
+    error = 0
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            error += 1
+            break
+
+    if error == 0:
+        is_Prime[i] = True
+
+while (1):
     n = int(input())
 
     if n == 0:
         break
-    for i in range(3, r):
-        if check[i] == True:
-            if check[n-i] == True:
-                print("%d = %d + %d" % (n, i, n-i))
+
+    for i in range(3, n):
+        if is_Prime[i] == True:
+            if is_Prime[n - i] == True:
+                print(f'{n} = {i} + {n - i}')
                 break
+
+# 출력시 f함수 사용법 익히기
