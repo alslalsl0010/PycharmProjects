ad3f81c5487dfe02e7a555a2e96ed382d10c0372
alslalsl0010 <alslalsl0010@naver.com>
2023-03-13 17:39:29 +0900

    N과M (2)_backtracking

--- pythonProject/15650.py
+++ pythonProject/15650.py
@@ -1,17 +1,20 @@
-n, m = list(map(int, input().split()))
-s = []
+import sys
 
 
-def dfs(start):
-    if len(s) == m:
-        print(' '.join(map(str, s)))
+def dfs(idx):
+    if len(answer) == m:
+        print(' '.join(map(str, answer)))
         return
 
-    for i in range(start, n+1):
-        if i not in s:
-            s.append(i)
-            dfs(i+1)
-            s.pop()
+    for i in range(idx, n + 1):
+        if i not in answer:
+            answer.append(i)
+            dfs(i + 1)  # 조심 idx 아님
+            answer.pop()
 
 
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+answer = []
 dfs(1)
