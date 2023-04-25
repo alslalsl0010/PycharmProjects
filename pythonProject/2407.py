167219c3a2917631714adec3bb3bce92a6cf8c8a
alslalsl0010 <alslalsl0010@naver.com>
2023-04-25 14:39:21 +0900

    조합

---
+++ pythonProject/2407.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    num = 1
+    for i in range(2, n+1):
+        num *= i
+    return num
+
+
+n, m = map(int, input().split())
+print(factorial(n) // factorial(n-m) * factorial(m))
